dd01fcd8453da8b3378b8932a74292df69b8c49c
Nerdhannn <ramadhananggadev@gmail.com>
2022-06-12 13:00:13 +0000

    X01BD: Initial bringup of xdroidOSS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/xdroid_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    xdroid_X01BD-userdebug
